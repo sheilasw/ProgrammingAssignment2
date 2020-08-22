bbcf7a176b7698e8f492390a31efe42e9be3ebb6
sheilasw <skcl@graniteandwater.net>
2020-08-21 17:42:45 -0700

    initial draft of makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m.x = matrix()) {
+        # test that the matrix is square
+        var.matrixdim <- dim(x)
+        if(var.matrixdim[1] != var.matrixdim[2]) {
+                message("makeCacheMatrix requires a square matrix (same # rows and # columns")
+                exit()
+        }
+        
+        # stores the original matrix used. For comparison to see whether the matrix being
+        # supplied matches the matrix that was supplied last time
+        set.orig <- function(m.y) {
+                matrix.orig <<- m.y
+                # if the original matrix is being stored, then it's a new one. Removed cached 
+                # inverted matrix
+                matrix.inv <<- NULL
+        }
+        get.orig <- function() matrix.input
+        
+        set.inv <- function(m.z) {
+                matrix.inv <<- m.z
+        }
+        get.inv <- function() matrix.inv
+        
+        list(set.orig = set.orig,
+             get.orig = get.orig,
+             set.inv = set.inv,
+             get.inv = get.inv)
 }
 
 
